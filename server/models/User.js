047737d2fe1aeb5dcb33baf517637fff75640f69
iamrens <rens.q.ebanculla@gmail.com>
2023-08-22 15:21:12 +0800

    fix the user schema validation

--- server/models/User.js
+++ server/models/User.js
@@ -3,16 +3,16 @@ import mongoose from "mongoose";
 const UserSchema = new mongoose.Schema({
     username: {
         type: String,
-        required: true,
+        minLength: [2, "Username should contain at least 2~15 characters"],
+        maxLength: [15, "Username should contain at least 2~15 characters"],
         unique: true,
-        min: 2,
-        max: 50,
+        required: true,
     },
     password: {
         type: String,
-        required: true,
         min: 8,
         max: 50,
+        required: true,
     },
     email: {
         type: String,
